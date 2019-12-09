ee300b22ea27177a99f5cee87dc09eda8c089f0f
Masafumi Yokoyama <myokoym@gmail.com>
2019-12-09 23:00:56 +0900

    Extract game.rb

---
+++ game.rb
@@ -0,0 +1,32 @@
+class Game
+  def initialize
+    font = Image[:font].slice_tiles(94, 5)
+    @font_num = font.slice(94 * 2 + 15, 10)
+    @font_kana = font.slice(94 * 3, 94)
+  end
+
+  def draw_num(x, y, num, digit=nil)
+    text = num.to_s
+    padding_size = 0
+    if text.size < digit
+      padding_size = digit - text.size
+      padding_size.times do |i|
+        Window.draw(x + i * 8 * 2, y, @font_kana[-1])
+      end
+    end
+    text.each_char.with_index do |char, i|
+      Window.draw(x + (i + padding_size) * 8 * 2, y, @font_num[char.to_i])
+    end
+  end
+
+  def draw_kana(x, y, text)
+    text.each_char.with_index do |char, i|
+      if char == " "
+        index = -1
+      else
+        index = char.ord - KANA_CODEPOINT_BASE
+      end
+      Window.draw(x + i * 8 * 2, y, @font_kana[index])
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,43 +1,12 @@
 require "dxopal"
 include DXOpal
 
+require_remote "game.rb"
+
 GROUND_Y = 400
 KANA_CODEPOINT_BASE = "ぁ".ord
 Image.register(:akazukin, "images/akazukin.png")
 Image.register(:font, "font/misaki_gothic_x2.png")
-
-class Game
-  def initialize
-    font = Image[:font].slice_tiles(94, 5)
-    @font_num = font.slice(94 * 2 + 15, 10)
-    @font_kana = font.slice(94 * 3, 94)
-  end
-
-  def draw_num(x, y, num, digit=nil)
-    text = num.to_s
-    padding_size = 0
-    if text.size < digit
-      padding_size = digit - text.size
-      padding_size.times do |i|
-        Window.draw(x + i * 8 * 2, y, @font_kana[-1])
-      end
-    end
-    text.each_char.with_index do |char, i|
-      Window.draw(x + (i + padding_size) * 8 * 2, y, @font_num[char.to_i])
-    end
-  end
-
-  def draw_kana(x, y, text)
-    text.each_char.with_index do |char, i|
-      if char == " "
-        index = -1
-      else
-        index = char.ord - KANA_CODEPOINT_BASE
-      end
-      Window.draw(x + i * 8 * 2, y, @font_kana[index])
-    end
-  end
-end
 
 Window.load_resources do
   game = Game.new
